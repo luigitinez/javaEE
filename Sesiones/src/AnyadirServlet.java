11e981eaf2d899ce5f0ec2e0eb6348af5d4a2042
Luis <lmgspain@hotmail.com>
2017-02-16 20:10:41 +0100

    Finalizado proyecto con Sesiones

---
+++ Sesiones/src/AnyadirServlet.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/anyadir")
+public class AnyadirServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public AnyadirServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String atributo = request.getParameter("atributo");
+		String valor = request.getParameter("valor");
+		HttpSession sesion = request.getSession();
+		sesion.setAttribute(atributo, valor);
+		
+		Enumeration nombresAtributos = sesion.getAttributeNames();
+	
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Sesion de Usuario</title>");
+		out.println("<meta charset='utf-8'/>");
+		out.println("</head>");
+		out.println("<body<");
+		out.println("<h1>Contenido de la sesion</h1>");
+		while(nombresAtributos.hasMoreElements()){
+			String nombreAtributo = (String)nombresAtributos.nextElement();
+			String valorAtributo = (String) sesion.getAttribute(nombreAtributo);
+			out.println(nombreAtributo + " - " + valorAtributo + "<br>");
+		}
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
