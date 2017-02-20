e9833f776a8355d74742dcfc641eef5a33a53e8e
Luis <lmgspain@hotmail.com>
2017-02-20 20:08:05 +0100

    asdf

---
+++ Encuesta/src/InvalidarSesionServlet.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/invalidar")
+public class InvalidarSesionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public InvalidarSesionServlet() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Sesiones</title>");
+		out.println("<meta charset='utf-8'/>");
+		out.println("</head>");
+		out.println("<body<");
+		out.println("<h1>Sessiones</h1>");
+		out.println("<p>Hemos cerrados la sesion</p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
